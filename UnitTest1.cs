4fafa86e48ef21d7a819987bec7c2063ffbb854e
Jacob Lehenbauer <jake@lehenbauer.us>
2026-03-11 00:59:15 -0500

    Add all property values to positive validation tests.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -38,8 +38,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.H, billNumber.BillChamber);
+            Assert.Equal(BillType.B, billNumber.BillType);
+            Assert.Equal(4, billNumber.Number);
             Assert.Equal("HB00004", billNumber.BillNumberLong());
             Assert.Equal("HB4", billNumber.BillNumberShort());
+            Assert.Equal("House Bill 4", billNumber.BillFullName());
         }
 
         [Fact]
@@ -51,8 +55,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.S, billNumber.BillChamber);
+            Assert.Equal(BillType.R, billNumber.BillType);
+            Assert.Equal(123, billNumber.Number);
             Assert.Equal("SR00123", billNumber.BillNumberLong());
             Assert.Equal("SR123", billNumber.BillNumberShort());
+            Assert.Equal("Senate Resolution 123", billNumber.BillFullName());
         }
 
         [Fact]
@@ -63,8 +71,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.H, billNumber.BillChamber);
+            Assert.Equal(BillType.JR, billNumber.BillType);
+            Assert.Equal(1374, billNumber.Number);
             Assert.Equal("HJR01374", billNumber.BillNumberLong());
             Assert.Equal("HJR1374", billNumber.BillNumberShort());
+            Assert.Equal("House Joint Resolution 1374", billNumber.BillFullName());
         }
 
         [Fact]
@@ -76,8 +88,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.S, billNumber.BillChamber);
+            Assert.Equal(BillType.CR, billNumber.BillType);
+            Assert.Equal(12, billNumber.Number);
             Assert.Equal("SCR00012", billNumber.BillNumberLong());
             Assert.Equal("SCR12", billNumber.BillNumberShort());
+            Assert.Equal("Senate Concurrent Resolution 12", billNumber.BillFullName());
         }
 
         [Fact]
@@ -89,8 +105,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.S, billNumber.BillChamber);
+            Assert.Equal(BillType.B, billNumber.BillType);
+            Assert.Equal(7, billNumber.Number);
             Assert.Equal("SB00007", billNumber.BillNumberLong());
             Assert.Equal("SB7", billNumber.BillNumberShort());
+            Assert.Equal("Senate Bill 7", billNumber.BillFullName());
         }
 
         [Fact]
@@ -102,8 +122,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.S, billNumber.BillChamber);
+            Assert.Equal(BillType.JR, billNumber.BillType);
+            Assert.Equal(45, billNumber.Number);
             Assert.Equal("SJR00045", billNumber.BillNumberLong());
             Assert.Equal("SJR45", billNumber.BillNumberShort());
+            Assert.Equal("Senate Joint Resolution 45", billNumber.BillFullName());
         }
 
         [Fact]
@@ -115,8 +139,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.H, billNumber.BillChamber);
+            Assert.Equal(BillType.CR, billNumber.BillType);
+            Assert.Equal(9, billNumber.Number);
             Assert.Equal("HCR00009", billNumber.BillNumberLong());
             Assert.Equal("HCR9", billNumber.BillNumberShort());
+            Assert.Equal("House Concurrent Resolution 9", billNumber.BillFullName());
         }
 
         [Fact]
@@ -128,8 +156,12 @@ namespace Bill_Number_Tests
             // Assert
             Assert.True(billNumber.IsValidBillNumber());
             Assert.True(billNumber.IsValid);
+            Assert.Equal(Chamber.H, billNumber.BillChamber);
+            Assert.Equal(BillType.R, billNumber.BillType);
+            Assert.Equal(100, billNumber.Number);
             Assert.Equal("HR00100", billNumber.BillNumberLong());
             Assert.Equal("HR100", billNumber.BillNumberShort());
+            Assert.Equal("House Resolution 100", billNumber.BillFullName());
         }
 
         [Fact]
